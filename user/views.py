765fd5cf318ffe6acb415e5e48f5a243f1ea5d8b
taekyu32-lee <taekyu32@gmail.com>
2022-10-14 16:59:21 +0900

    signup

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request,'signup.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        phone_number = request.POST.get('phone')
+        addr = request.POST.get('address')
+
+        UserModel.objects.create_user(username=username,password=password,phone_number=phone_number,addr=addr)
+
+        print(UserModel.objects.all)
+
+        return redirect('/signin')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
